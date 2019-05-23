cacebd8ea608b96aa28d5e2f78bba78f6403c7d1
drsalmon <46974493+drsalmon@users.noreply.github.com>
2019-05-23 11:58:03 -0500

    4

---
+++ DanielSalazar_Final_16.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def cordenadax(x):
+    for i in range(x):
+        x[i] = 40.2
+    return x
+
+
+def cordenaday(x):
+    for i in range(x):
+        x[i] = 40.2
+    return x
+
+def hora(x):
+    for i in range(x):
+        x[i] = 40.2
+    return x
+
+def velocidad(x):
+    for i in range(x):
+        x[i] = 40.2
+    return x
